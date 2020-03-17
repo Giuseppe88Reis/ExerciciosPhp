684814a868871c8ca4ddd50b05a455ae464de687
Giuseppe88Reis <62251234+Giuseppe88Reis@users.noreply.github.com>
2020-03-17 15:27:33 -0300

    Resolução_Giuseppe.php

--- src/TextWrap/TextWrapInterface.php
+++ src/TextWrap/TextWrapInterface.php
@@ -1,39 +1,33 @@
 <?php
 
 namespace Galoa\ExerciciosPhp\TextWrap;
-
-/**
- * Define uma interface para o exercício de quebra de linha.
- */
+ 
 interface TextWrapInterface {
 
-  /**
-   * Quebra uma string em diversas strings com tamanho passado por parâmetro.
-   *
-   * Suponha que você tenha uma string com um texto bastante longo. Você quer
-   * imprimir na tela todo o texto, mas garantir um limite máximo de N
-   * caracteres por linha.
-   *
-   * Alguns pontos que você deve ter em mente:
-   * - Retorne o todo o texto, com o máximo de palavras por linha, mas sem
-   *   nunca extrapolar o limite de caracteres.
-   * - Se uma palavra não couber na linha e o comprimento dela for menor que o
-   *   limite de caracteres, ela não deve ser cortada, e sim jogada para a
-   *   próxima linha.
-   * - Se a palavra for maior que o limite de caracteres por linha, corte a
-   *   palavra e continue a imprimi-la na linha seguinte.
-   * - Não utilize funções prontas, como p.ex. o wordwrap do PHP. O objetivo
-   *   deste exercício é que você desenvolva o algoritmo indicado.
-   *
-   * @param string $text
-   *   O texto que será utilizado como entrada.
-   * @param int $length
-   *   Em quantos caracteres a linha deverá ser quebrada.
-   *
-   * @return array
-   *   Um array de strings equivalente ao texto recebido por parâmetro porém
-   *   respeitando o comprimento de linha e as regras especificadas acima.
-   */
+ public function metodOne( $text );
+ public function metodTwo( $text, int $lenght );
+ echo wordwrap($texto, 30, "<br />\n");
+}
+
+class oopClass implements oopinterface {
+ 
+ public function methdOne( $text )
+ {
+   echo 'O Primeiro texto digitado foi:'. $text. '<br>';
+  }
+ 
+ public function methdTwo( $text, int $lenght )
+ {
+  
+  echo 'O Segundo texto digitado é:'. $text. '<br>';
+  echo 'O número escolhido será:'. $lenght. '<br>';
+ }
+  wordwrap($texto, 20, "<br />\n", true);
+  $array = array("$text", "$lenght");
+  var_dump($array);
+
+
+  wordwrap($texto, 20, "<br />\n", true);
   public function textWrap(string $text, int $length): array;
 
 }
